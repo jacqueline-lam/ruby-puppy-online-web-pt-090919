11aab106b1bdd1e0617da37003859a060bb23cc6
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-07 14:28:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @@all << self
+    @name = name
+  end
+  
+  def save
+    # @@all << sel
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.clear_all
+   @@all = []
+  end
+  
+end
